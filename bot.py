d7adb79ddbd4f1d3793705fd4b7d9d067f13637d
Skele <faisal2193f@gmail.com>
2023-11-26 20:23:25 +0530

    Made better help command

--- bot.py
+++ bot.py
@@ -11,10 +11,52 @@ token = str(os.getenv("TOKEN")) # now import your token
 custom_prefix = 'h!'
 Helper = bridge.Bot(command_prefix=commands.when_mentioned_or(custom_prefix, custom_prefix.upper()), intents=intents)
 
+class MyHelp(commands.HelpCommand):
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title="Help", color=0x1fe2f3, description="Here are the available commands:")
+        embed.set_author(name=f"@{Helper.user}", icon_url=f"{Helper.user.avatar}")
+        embed.set_thumbnail(url=f"{Helper.user.avatar}")
+
+        prefix = self.context.clean_prefix
+
+        # Modify this part to change 'No Category' appearance
+        no_category_commands = mapping.pop(None, None)  # Remove 'No Category' from mapping
+        if no_category_commands:
+            command_names = list({f"`{c.qualified_name}`" if hasattr(c, 'qualified_name') else "`Unnamed Command`" for c in no_category_commands})
+            if command_names:
+                embed.add_field(name="**Miscellaneous:**", value=", ".join(command_names), inline=False)
+
+        for cog, commands in mapping.items():
+            command_names = list({f"`{c.qualified_name}`" if hasattr(c, 'qualified_name') else "`Unnamed Command`" for c in commands})
+            if command_names:
+                cog_name = getattr(cog, "qualified_name", "No Category")
+                embed.add_field(name=f"**{cog_name}:**", value=", ".join(command_names), inline=False)
+
+        embed.set_footer(text=f"Use {prefix}help [command] for more details on a specific command.")
+
+        channel = self.get_destination()
+        await channel.send(embed=embed)
+
+    async def send_error_message(self, error):
+        embed = discord.Embed(title="Error!", description=error, color=discord.Color.red())
+        channel = self.get_destination()
+
+        await channel.send(embed=embed)
+
+    async def send_command_help(self, command):
+      embed = discord.Embed(
+          title=f"Help for `{self.get_command_signature(command)}`",
+          description=command.description or "No help available.",
+          color=discord.Color.blue()
+      )
+      await self.get_destination().send(embed=embed)
+
+Helper.help_command = MyHelp()
+
 # This prints in terminal that the bot is online
 @Helper.event
 async def on_ready():
-    await Helper.change_presence(status=discord.Status.online, activity=discord.Game("Minecraft!"))
+    await Helper.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="Your Mom!"))
     print(f"Logged in as {Helper.user}.")
 
 @Helper.event
