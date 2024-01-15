c8b52f5e7a92976084aded2d86c3ef2f7275cbb1
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-01-15 18:21:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,51 +7,11 @@ import dotenv # import .env to to use your token
 intents = discord.Intents.all()
 
 dotenv.load_dotenv() # now load the .evn file
-token = str(os.getenv("TOKEN")) # now import your token
+token = str(os.getenv("TEST_TOKEN")) # now import your token
 custom_prefix = 'h!'
 Helper = bridge.Bot(command_prefix=commands.when_mentioned_or(custom_prefix, custom_prefix.upper()), intents=intents)
 
-class MyHelp(commands.HelpCommand):
-    async def send_bot_help(self, mapping):
-        embed = discord.Embed(title="Help", color=0x1fe2f3, description="Here are the available commands:")
-        embed.set_author(name=f"@{Helper.user}", icon_url=f"{Helper.user.avatar}")
-        embed.set_thumbnail(url=f"{Helper.user.avatar}")
-
-        prefix = self.context.clean_prefix
-
-        # Modify this part to change 'No Category' appearance
-        no_category_commands = mapping.pop(None, None)  # Remove 'No Category' from mapping
-        if no_category_commands:
-            command_names = list({f"`{c.qualified_name}`" if hasattr(c, 'qualified_name') else "`Unnamed Command`" for c in no_category_commands})
-            if command_names:
-                embed.add_field(name="**Miscellaneous:**", value=", ".join(command_names), inline=False)
-
-        for cog, commands in mapping.items():
-            command_names = list({f"`{c.qualified_name}`" if hasattr(c, 'qualified_name') else "`Unnamed Command`" for c in commands})
-            if command_names:
-                cog_name = getattr(cog, "qualified_name", "No Category")
-                embed.add_field(name=f"**{cog_name}:**", value=", ".join(command_names), inline=False)
-
-        embed.set_footer(text=f"Use {prefix}help [command] for more details on a specific command.")
-
-        channel = self.get_destination()
-        await channel.send(embed=embed)
-
-    async def send_error_message(self, error):
-        embed = discord.Embed(title="Error!", description=error, color=discord.Color.red())
-        channel = self.get_destination()
-
-        await channel.send(embed=embed)
-
-    async def send_command_help(self, command):
-      embed = discord.Embed(
-          title=f"Help for `{self.get_command_signature(command)}`",
-          description=command.description or "No help available.",
-          color=discord.Color.blue()
-      )
-      await self.get_destination().send(embed=embed)
-
-Helper.help_command = MyHelp()
+Helper.remove_command("help")
 
 # This prints in terminal that the bot is online
 @Helper.event
@@ -61,21 +21,15 @@ async def on_ready():
 
 @Helper.event
 async def on_message(message):
-    introduction_channel = 1234567890 # Your introduction channel id
-    if message.channel.id == introduction_channel:
-        await message.add_reaction('🙋‍♀️')
-    polls_channel = 1234567890 # Your polls channel id
-    if message.channel.id == polls_channel:
-        await message.add_reaction('👍')
-        await message.add_reaction('👎')
+    if '<@875208986603958344>' in message.content:
+        emoji_name = 'pewpew'
+        emoji = discord.utils.get(Helper.emojis, name=emoji_name)
+        if emoji:
+            await message.add_reaction(emoji)
+        else:
+            print(f"Emoji '{emoji_name}' not found.")
 
     await Helper.process_commands(message)
-
-@Helper.event
-async def on_member_join(ctx):
-    member_role_name = "Member"
-    member_role = discord.utils.get(ctx.guild.roles, name=member_role_name)
-    await ctx.add_roles(member_role)
 
 # Define a function to load cogs from the 'Cogs' directory
 def load_cogs():
@@ -87,8 +41,7 @@ def load_cogs():
                 print(f'{cog} has been loaded')
             except Exception as e:
                 print(f"Error loading extension '{cog}': {e}")
-                
+
 load_cogs()
 
 Helper.run(token)
-#testing to see if push works
