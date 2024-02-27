0e19c9927ed527c4d3264592a417236aa371071c
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-02-27 20:41:22 +0530

    Migrated to mongodb

--- Cogs/LogsSystem.py
+++ Cogs/LogsSystem.py
@@ -1,207 +1,252 @@
 import discord
 from discord.ext import commands
-from helperdb import Database
 import datetime
+import os
+import dotenv
+import pymongo
+
+dotenv.load_dotenv()
+uri = str(os.getenv("MONGO"))
+
+client = pymongo.MongoClient(uri)
+db = client["Main"]
 
 class LogsSystem(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.db = Database("database/server.db")
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        await self.db.connect()
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
+
         if before.embeds:
             return
+        
+        collection = db["message_logs"]
 
-        query = "SELECT channel_id FROM message_logs WHERE guild_id =?"
-        result = await self.db.fetchone(query, before.guild.id)
-        if result:
-            channel_id = result[0]
-            channel = self.bot.get_channel(channel_id)
+        data = collection.find_one({"_id": before.guild.id})
 
-            if channel:
-                embed = discord.Embed(
-                    title="Message Edited",
-                    description=f"Message edited in {before.channel.mention}",
-                    color=discord.Color.blurple(),
-                    timestamp=datetime.datetime.utcnow()
-                )
-                embed.set_author(name=f"@{before.author}", icon_url=before.author.avatar)
-                embed.set_thumbnail(url=before.author.avatar)
-                embed.add_field(name="Before", value=before.content, inline=False)
-                embed.add_field(name="After", value=after.content, inline=False)
+        if not data:
+            return
+        
+        channel = self.bot.get_channel(data["channel"])
 
-                await channel.send(embed=embed)
+        if not channel:
+            return
+
+        message_link = f"https://discord.com/channels/{before.guild.id}/{before.channel.id}/{before.id}"
+        
+        embed = discord.Embed(
+            title="Message Edited",
+            description=f"Message edited in {before.channel.mention} [Link]({message_link}).",
+            color=discord.Color.blurple(),
+            timestamp=datetime.datetime.utcnow()
+        )
+        embed.set_author(name=f"@{before.author}", icon_url=before.author.avatar)
+        embed.set_thumbnail(url=before.author.avatar)
+        embed.add_field(name="Before", value=before.content, inline=False)
+        embed.add_field(name="After", value=after.content, inline=False)
+
+        await channel.send(embed=embed)
         
     @commands.Cog.listener()
     async def on_message_delete(self, message):
         if message.embeds:
             return
         
-        query = "SELECT channel_id FROM message_logs WHERE guild_id =?"
-        result = await self.db.fetchone(query, message.guild.id)
-        if result:
-            channel_id = result[0]
-            channel = self.bot.get_channel(channel_id)
+        collection = db["message_logs"]
 
-            if channel:
-                embed = discord.Embed(
-                    title="Message Deleted",
-                    description=f"Message deleted in {message.channel.mention}",
-                    color=discord.Color.red(),
-                    timestamp=datetime.datetime.utcnow()
-                )
-                embed.set_author(name=f"@{message.author}", icon_url=message.author.avatar)
-                embed.set_thumbnail(url=message.author.avatar)
-                embed.add_field(name="Message:", value=message.content, inline=False)
+        data = collection.find_one({"_id": message.guild.id})
 
-                await channel.send(embed=embed)
+        if not data:
+            return
+        
+        channel = self.bot.get_channel(data["channel"])
+
+        if not channel:
+            return
+        
+        embed = discord.Embed(
+            title="Message Deleted",
+            description=f"Message deleted in {message.channel.mention}.",
+            color=discord.Color.red(),
+            timestamp=datetime.datetime.utcnow()
+        )
+        embed.set_author(name=f"@{message.author}", icon_url=message.author.avatar)
+        embed.set_thumbnail(url=message.author.avatar)
+        embed.add_field(name="Message:", value=message.content, inline=False)
+
+        await channel.send(embed=embed)                
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        query = "SELECT channel_id FROM member_logs WHERE guild_id =?"
-        result = await self.db.fetchone(query, member.guild.id)
-        if result:
-            channel_id = result[0]
-            channel = self.bot.get_channel(channel_id)
 
-            if channel:
-                embed = discord.Embed(
-                    title="Member Joined",
-                    description=f"{member.mention} joined the server",
-                    color=discord.Color.green(),
-                    timestamp=datetime.datetime.utcnow()
-                )
+        collection = db["member_logs"]
 
-                account_created_at = member.created_at
-                account_age = int(account_created_at.timestamp())
+        data = collection.find_one({"_id": member.guild.id})
 
-                embed.set_author(name=f"@{member.name}", icon_url=member.avatar)
-                embed.set_thumbnail(url=member.avatar)
-                embed.add_field(name="User info:", value=f"ID: {member.id}\nName: {member.mention}", inline=False)
-                embed.add_field(name="Account Age:", value=f"<t:{account_age}:D> (<t:{account_age}:R>)", inline=False)
+        if not data:
+            return
+        
+        channel = self.bot.get_channel(data["channel"])
 
-                await channel.send(embed=embed)
+        if not channel:
+            return
+
+        embed = discord.Embed(
+            title="Member Joined",
+            description=f"{member.mention} joined the server",
+            color=discord.Color.green(),
+            timestamp=datetime.datetime.utcnow()
+        )
+
+        account_created_at = member.created_at
+        account_age = int(account_created_at.timestamp())
+
+        embed.set_author(name=f"@{member.name}", icon_url=member.avatar)
+        embed.set_thumbnail(url=member.avatar)
+        embed.add_field(name="User info:", value=f"ID: {member.id}\nName: {member.mention}", inline=False)
+        embed.add_field(name="Account Age:", value=f"<t:{account_age}:D> (<t:{account_age}:R>)", inline=False)
+
+        await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        query = "SELECT channel_id FROM member_logs WHERE guild_id =?"
-        result = await self.db.fetchone(query, member.guild.id)
-        if result:
-            channel_id = result[0]
-            channel = self.bot.get_channel(channel_id)
 
-            if channel:
-                embed = discord.Embed(
-                    title="Member Left",
-                    description=f"{member.mention} left the server",
-                    color=discord.Color.red(),
-                    timestamp=datetime.datetime.utcnow()
-                )
-                embed.set_author(name=f"@{member.name}", icon_url=member.avatar)
-                embed.set_thumbnail(url=member.avatar)
-                embed.add_field(name="User info:", value=f"ID: {member.id}\nName: {member.mention}", inline=False)
+        collection = db["member_logs"]
 
-                await channel.send(embed=embed)
+        data = collection.find_one({"_id": member.guild.id})
+
+        if not data:
+            return
+        
+        channel = self.bot.get_channel(data["channel"])
+
+        if not channel:
+            return
+        
+        embed = discord.Embed(
+            title="Member Left",
+            description=f"{member.mention} left the server",
+            color=discord.Color.red(),
+            timestamp=datetime.datetime.utcnow()
+        )
+        embed.set_author(name=f"@{member.name}", icon_url=member.avatar)
+        embed.set_thumbnail(url=member.avatar)
+        embed.add_field(name="User info:", value=f"ID: {member.id}\nName: {member.mention}", inline=False)
+
+        await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
-        query = "SELECT channel_id FROM member_logs WHERE guild_id =?"
-        result = await self.db.fetchone(query, before.guild.id)
-        if result:
-            channel_id = result[0]
-            channel = self.bot.get_channel(channel_id)
 
-            if channel:
-                if before.nick != after.nick:
-                    embed = discord.Embed(
-                        title="Nickname Changed",
-                        description=f"Nickname changed for {before.mention}",
-                        color=discord.Color.blurple(),
-                        timestamp=datetime.datetime.utcnow()
-                    )
-                    embed.set_author(name=f"@{before.name}", icon_url=before.avatar)
-                    embed.set_thumbnail(url=before.avatar)
-                    embed.add_field(name="Before", value=before.nick, inline=False)
-                    embed.add_field(name="After", value=after.nick, inline=False)
+        collection = db["member_logs"]
 
-                    await channel.send(embed=embed)
+        data = collection.find_one({"_id": before.guild.id})
 
-                if before.roles!= after.roles:
-                    added_roles = [role for role in after.roles if role not in before.roles]
-                    removed_roles = [role for role in before.roles if role not in after.roles]
+        if not data:
+            return
+        
+        channel = self.bot.get_channel(data["channel"])
 
-                    if added_roles:
-                        embed = discord.Embed(
-                            title="Role Added",
-                            description=f"Role added to {before.mention}",
-                            color=discord.Color.blurple(),
-                            timestamp=datetime.datetime.utcnow()
-                        )
-                        embed.set_author(name=f"@{before.name}", icon_url=before.avatar)
-                        embed.set_thumbnail(url=before.avatar)
-                        embed.add_field(name="Role:", value=", ".join(role.mention for role in added_roles), inline=False)
+        if not channel:
+            return
+        
+        if before.nick != after.nick:
+            embed = discord.Embed(
+                title="Nickname Changed",
+                description=f"Nickname changed for {before.mention}",
+                color=discord.Color.blurple(),
+                timestamp=datetime.datetime.utcnow()
+            )
+            embed.set_author(name=f"@{before.name}", icon_url=before.avatar)
+            embed.set_thumbnail(url=before.avatar)
+            embed.add_field(name="Before", value=before.nick, inline=False)
+            embed.add_field(name="After", value=after.nick, inline=False)
 
-                        await channel.send(embed=embed)
+            await channel.send(embed=embed)
 
-                    if removed_roles:
-                        embed = discord.Embed(
-                            title="Role Removed",
-                            description=f"Role removed from {before.mention}",
-                            color=discord.Color.blurple(),
-                            timestamp=datetime.datetime.utcnow()
-                        )
-                        embed.set_author(name=f"@{before.name}", icon_url=before.avatar)
-                        embed.set_thumbnail(url=before.avatar)
-                        embed.add_field(name="Role:", value=", ".join(role.mention for role in removed_roles), inline=False)
+        if before.roles!= after.roles:
+            added_roles = [role for role in after.roles if role not in before.roles]
+            removed_roles = [role for role in before.roles if role not in after.roles]
 
-                        await channel.send(embed=embed)
+            if added_roles:
+                embed = discord.Embed(
+                    title="Role Added",
+                    description=f"Role added to {before.mention}",
+                    color=discord.Color.blurple(),
+                    timestamp=datetime.datetime.utcnow()
+                )
+                embed.set_author(name=f"@{before.name}", icon_url=before.avatar)
+                embed.set_thumbnail(url=before.avatar)
+                embed.add_field(name="Role:", value=", ".join(role.mention for role in added_roles), inline=False)
+
+                await channel.send(embed=embed)
+
+            if removed_roles:
+                embed = discord.Embed(
+                    title="Role Removed",
+                    description=f"Role removed from {before.mention}",
+                    color=discord.Color.blurple(),
+                    timestamp=datetime.datetime.utcnow()
+                )
+                embed.set_author(name=f"@{before.name}", icon_url=before.avatar)
+                embed.set_thumbnail(url=before.avatar)
+                embed.add_field(name="Role:", value=", ".join(role.mention for role in removed_roles), inline=False)
+
+                await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_ban(self, guild, user):
-        query = "SELECT channel_id FROM member_logs WHERE guild_id =?"
-        result = await self.db.fetchone(query, guild.id)
-        if result:
-            channel_id = result[0]
-            channel = self.bot.get_channel(channel_id)
 
-            if channel:
-                embed = discord.Embed(
-                    title="Member Banned",
-                    description=f"{user.mention} was banned from the server",
-                    color=discord.Color.red(),
-                    timestamp=datetime.datetime.utcnow()
-                )
-                embed.set_author(name=f"@{user.name}", icon_url=user.avatar)
-                embed.set_thumbnail(url=user.avatar)
-                embed.add_field(name="User info:", value=f"ID: {user.id}\nName: {user.mention}", inline=False)
+        collection = db["member_logs"]
 
-                await channel.send(embed=embed)
+        data = collection.find_one({"_id": guild.id})
+
+        if not data:
+            return
+        
+        channel = self.bot.get_channel(data["channel"])
+
+        if not channel:
+            return
+        embed = discord.Embed(
+            title="Member Banned",
+            description=f"{user.mention} was banned from the server",
+            color=discord.Color.red(),
+            timestamp=datetime.datetime.utcnow()
+        )
+        embed.set_author(name=f"@{user.name}", icon_url=user.avatar)
+        embed.set_thumbnail(url=user.avatar)
+        embed.add_field(name="User info:", value=f"ID: {user.id}\nName: {user.mention}", inline=False)
+
+        await channel.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_unban(self, guild, user):
-        query = "SELECT channel_id FROM member_logs WHERE guild_id =?"
-        result = await self.db.fetchone(query, guild.id)
-        if result:
-            channel_id = result[0]
-            channel = self.bot.get_channel(channel_id)
 
-            if channel:
-                embed = discord.Embed(
-                    title="Member Unbanned",
-                    description=f"{user.mention} was unbanned from the server",
-                    color=discord.Color.green(),
-                    timestamp=datetime.datetime.utcnow()
-                )
-                embed.set_author(name=f"@{user.name}", icon_url=user.avatar)
-                embed.set_thumbnail(url=user.avatar)
-                embed.add_field(name="User info:", value=f"ID: {user.id}\nName: {user.mention}", inline=False)
+        collection = db["member_logs"]
 
-                await channel.send(embed=embed)
+        data = collection.find_one({"_id": guild.id})
+
+        if not data:
+            return
+        
+        channel = self.bot.get_channel(data["channel"])
+
+        if not channel:
+            return
+        
+        embed = discord.Embed(
+            title="Member Unbanned",
+            description=f"{user.mention} was unbanned from the server",
+            color=discord.Color.green(),
+            timestamp=datetime.datetime.utcnow()
+        )
+        embed.set_author(name=f"@{user.name}", icon_url=user.avatar)
+        embed.set_thumbnail(url=user.avatar)
+        embed.add_field(name="User info:", value=f"ID: {user.id}\nName: {user.mention}", inline=False)
+
+        await channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(LogsSystem(bot))
--- Cogs/Server.py
+++ Cogs/Server.py
@@ -1,145 +1,39 @@
 import discord
 from discord.ext import bridge, commands
-from helperdb import Database
 import datetime
 import os
 import dotenv
 import asyncio
+import pymongo
 
 dotenv.load_dotenv()
 guild_id = int(os.getenv("GUILD_ID"))
+uri = str(os.getenv("MONGO"))
+
+client = pymongo.MongoClient(uri)
+db = client["Main"]
 
 class ServerSetup(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.db = Database("database/server.db")
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        await self.db.connect()
-        await self.create_tables()
-
-    async def create_tables(self):
-        welcomer_table = """CREATE TABLE IF NOT EXISTS welcomer (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            guild_id INTEGER,
-            channel_id INTEGER,
-            role_id INTEGER
-        )"""
-
-        await self.db.execute(welcomer_table)
-
-        introduction_table = """CREATE TABLE IF NOT EXISTS introduction (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            guild_id INTEGER,
-            channel_id INTEGER
-        )"""
-
-        await self.db.execute(introduction_table)
-
-        message_logs_table = """CREATE TABLE IF NOT EXISTS message_logs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            guild_id INTEGER,
-            channel_id INTEGER
-        )"""
-
-        member_logs_table = """CREATE TABLE IF NOT EXISTS member_logs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            guild_id INTEGER,
-            channel_id INTEGER
-        )"""
-
-        vc_logs_table = """CREATE TABLE IF NOT EXISTS vc_logs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            guild_id INTEGER,
-            channel_id INTEGER
-        )"""
-
-        server_logs_table = """CREATE TABLE IF NOT EXISTS server_logs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            guild_id INTEGER,
-            channel_id INTEGER
-        )"""
-
-        misc_logs_table = """CREATE TABLE IF NOT EXISTS misc_logs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            guild_id INTEGER,
-            channel_id INTEGER
-        )"""
-
-        await self.db.execute(message_logs_table)
-        await self.db.execute(member_logs_table)
-        await self.db.execute(vc_logs_table)
-        await self.db.execute(server_logs_table)
-        await self.db.execute(misc_logs_table)
 
     @bridge.bridge_command(guild_id=guild_id, name="setwelcomer", description="Sets the welcomer channel and role")
     @commands.has_permissions(administrator=True)
-    async def set_welcomer(self, ctx, channel: discord.TextChannel, role: discord.Role):
+    async def set_welcomer(self, ctx, channel: discord.TextChannel):
+        collection = db["welcomer"]
         
         if not channel:
-            await ctx.respond("Please specify a channel!")
-            return
-
-        if not role:
-            await ctx.respond("Please specify a role!")
-            return
-
-        query = "SELECT * FROM welcomer WHERE guild_id =?"
-        existing_data = await self.db.fetchone(query, ctx.guild.id)
+            channel = ctx.channel
         
-        if existing_data:
-            existing_channel_id = existing_data["channel_id"]
-            existing_role_id = existing_data["role_id"]
-
-            if existing_channel_id == channel.id and existing_role_id == role.id:
-                await ctx.respond("The welcomer channel and role are already set!")
-                return
-            else:
-                update_query = "UPDATE welcomer SET channel_id =?, role_id =? WHERE guild_id =?"
-                await self.db.execute(update_query, channel.id, role.id, ctx.guild.id)
-        
-        else:
-            insert_query = "INSERT INTO welcomer (guild_id, channel_id, role_id) VALUES (?,?,?)"
-            await self.db.execute(insert_query, ctx.guild.id, channel.id, role.id)
+        collection.update_one(
+            {"_id": ctx.guild.id},
+            {"$set": {"channel": channel.id}},
+            upsert=True
+        )
 
         embed = discord.Embed(
             title="Welcomer Set",
-            description=f"The welcomer channel has been set to {channel.mention} and the role has been set to {role.mention}",
-            color=0x1fe2f3,
-            timestamp=datetime.datetime.utcnow()
-        )
-
-        await ctx.respond(embed=embed)
-
-    @bridge.bridge_command(guild_id=guild_id, name="setintroduction", description="Sets the introduction channel")
-    @commands.has_permissions(administrator=True)
-    async def set_introduction(self, ctx, channel: discord.TextChannel):
-
-        if not channel:
-            await ctx.respond("Please specify a channel!")
-            return
-        
-        query = "SELECT * FROM introduction WHERE guild_id =?"
-        existing_data = await self.db.fetchone(query, ctx.guild.id)
-
-        if existing_data:
-            existing_channel_id = existing_data["channel_id"]
-
-            if existing_channel_id == channel.id:
-                await ctx.respond("The introduction channel is already set!")
-                return
-            else:
-                update_query = "UPDATE introduction SET channel_id =? WHERE guild_id =?"
-                await self.db.execute(update_query, channel.id, ctx.guild.id)
-
-        else:
-            insert_query = "INSERT INTO introduction (guild_id, channel_id) VALUES (?,?)"
-            await self.db.execute(insert_query, ctx.guild.id, channel.id)
-
-        embed = discord.Embed(
-            title="Introduction Set",
-            description=f"The introduction channel has been set to {channel.mention}",
+            description=f"The welcomer channel has been set to {channel.mention}.",
             color=0x1fe2f3,
             timestamp=datetime.datetime.utcnow()
         )
@@ -150,7 +44,7 @@ class ServerSetup(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def set_logs(self, ctx):
 
-        tables = {
+        collections = {
             "message_logs": "Message Logs",
             "member_logs": "Member Logs",
             "vc_logs": "Voice Channel Logs",
@@ -158,11 +52,10 @@ class ServerSetup(commands.Cog):
             "misc_logs": "Miscellaneous Logs"
         }
 
-        for table, title in tables.items():
+        for collection_name, title in collections.items():
             await ctx.respond(f"Please specify {title} channel ID or mention (type `skip` to leave it blank):")
 
             try:
-
                 message = await self.bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=60)
 
                 if message.content.lower() == "skip":
@@ -171,8 +64,12 @@ class ServerSetup(commands.Cog):
                     channel = await commands.TextChannelConverter().convert(ctx, message.content)
                     channel_id = channel.id
 
-                update_query = f"INSERT OR REPLACE INTO {table} (guild_id, channel_id) VALUES (?,?)"
-                await self.db.execute(update_query, ctx.guild.id, channel_id)
+                collection = db[collection_name]
+                collection.update_one(
+                    {"_id": ctx.guild.id},
+                    {"$set": {"channel": channel_id}},
+                    upsert=True
+                )
 
                 if channel_id:
                     embed = discord.Embed(
@@ -199,44 +96,22 @@ class ServerSetup(commands.Cog):
 class Server(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.db = Database("database/server.db")
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        await self.db.connect()
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        query = "SELECT * FROM welcomer WHERE guild_id =?"
-        data = await self.db.fetchone(query, member.guild.id)
+        collection = db["welcomer"]
 
-        if data:
-            channel_id = data["channel_id"]
-            role_id = data["role_id"]
+        data = collection.find_one({"_id": member.guild.id})
 
-            channel = member.guild.get_channel(channel_id)
-            role = member.guild.get_role(role_id)
-            await channel.send(f"Welcome to the server, {member.mention}!")
-            await member.add_roles(role)
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.bot:
+        if not data:
             return
         
-        query_introduction = "SELECT * FROM introduction WHERE guild_id =?"
-        data = await self.db.fetchone(query_introduction, message.guild.id)
+        channel = self.bot.get_channel(data["channel"])
 
-        if data:
-            channel_id = data["channel_id"]
-
-            if message.channel.id == channel_id:
-                await message.add_reaction("🙋‍♀️")
-                return
-            else:
-                return
+        if not channel:
+            return
         
-        await self.bot.process_commands(message)
+        await channel.send(f"Welcome to the server, {member.mention}!")
 
 def setup(bot):
     bot.add_cog(ServerSetup(bot))
