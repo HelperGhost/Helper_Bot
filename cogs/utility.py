7fd736027bcfabbca390d1ab505613b4a2c6e1d5
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-02-28 22:58:15 +0530

    Made a hybrid command

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,5 +1,4 @@
 from discord.ext import commands
-from discord import app_commands
 import discord
 
 guild_id = 1193857532813381653
@@ -10,10 +9,10 @@ class Utility(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="ping", description="Tells the ping of the bot.")
-    async def ping(self, interaction: discord.Interaction):
+    @commands.hybrid_command(name="ping", description="Tells the ping of the bot.")
+    async def ping(self, ctx: commands.Context):
         latency = round(self.bot.latency * 1000)
-        await interaction.response.send_message(f"Pong! I got a ping of {latency}ms.")
+        await ctx.send(f"Pong! I got a ping of {latency}ms.")
 
 async def setup(bot):
     await bot.add_cog(Utility(bot))
