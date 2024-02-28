4c286eafb2b1990a2093b411f61443b15a3445b1
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-02-28 22:19:18 +0530

    Changed the code a little bit

--- bot.py
+++ bot.py
@@ -1,28 +1,32 @@
-#!/usr/bin/env python3
-
-from discord.ext import commands
 import discord
-import config
+from discord.ext import commands
+import os
+import dotenv
+
+dotenv.load_dotenv()
+token = os.getenv("TOKEN")
 
 class Bot(commands.Bot):
     def __init__(self, intents: discord.Intents, **kwargs):
-        super().__init__(command_prefix=commands.when_mentioned_or('$'), intents=intents, **kwargs)
+        super().__init__(command_prefix=commands.when_mentioned_or("!"), intents=intents, **kwargs)
 
     async def setup_hook(self):
-        for cog in config.cogs:
+        for file in os.listdir("cogs"):
             try:
-                await self.load_extension(cog)
-            except Exception as exc:
-                print(f'Could not load extension {cog} due to {exc.__class__.__name__}: {exc}')
+                if not file.endswith(".py"):
+                    return
+                if file == "__init__.py":
+                    return
+                cog = file[:-3]
+                self.load_extension(f"cogs.{cog}")
+            except Exception as e:
+                print(f"Failed to load {cog} due to {e.__class__.__name__}: {e}")
 
     async def on_ready(self):
-        print(f'Logged on as {self.user} (ID: {self.user.id})')
+        print(f"Logged on as {self.user}.")
 
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = Bot(intents=intents)
+intents = discord.Intents.all()
+bot = Bot(intents=intents) 
 
-# write general commands here
-
-bot.run(config.token)
+bot.run(token)
