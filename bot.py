e5f599ca4631ab58f4ff9b601ffff42c773ae4d5
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-02-28 23:12:11 +0530

    Updated the status of the bot

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ class Bot(commands.Bot):
     def __init__(self, intents: discord.Intents, **kwargs):
         super().__init__(command_prefix=commands.when_mentioned_or("!"), intents=intents, **kwargs)
 
+
     async def setup_hook(self):
         for file in os.listdir("cogs"):
             try:
@@ -22,12 +23,15 @@ class Bot(commands.Bot):
             except Exception as e:
                 print(f"Failed to load {cog} due to {e.__class__.__name__}: {e}")
 
+
     async def on_ready(self):
+        await self.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="Your Mom S2!"))
         print(f"Logged on as {self.user}.")
         await self.tree.sync()
 
 
 intents = discord.Intents.all()
 bot = Bot(intents=intents) 
+
 
 bot.run(token)
